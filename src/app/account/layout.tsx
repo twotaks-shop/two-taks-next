422ec30a321e14add1ad8c14a6a622e99b43240a
Daury Pérez <dauryjey@gmail.com>
2025-06-18 21:19:57 -0400

    now customers can access to their subscriptions

--- src/app/account/layout.tsx
+++ src/app/account/layout.tsx
@@ -45,6 +45,25 @@ const accountNavItems = [
 			</svg>
 		),
 	},
+	{
+		href: "https://shopify.com/76742426880/account/pages/6971b1a1-27f6-4c27-b8b0-3009fd3b921d",
+		label: "Subscriptions",
+		icon: (
+			<svg
+				className="w-5 h-5"
+				fill="none"
+				stroke="currentColor"
+				viewBox="0 0 24 24"
+			>
+				<path
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					strokeWidth={2}
+					d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+				/>
+			</svg>
+		),
+	},
 ];
 
 export default function AccountLayout({
